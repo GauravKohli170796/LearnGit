9eccbd8478981f9659cf012a026233228b07fc85
GauravKohli170796 <89524181+GauravKohli170796@users.noreply.github.com>
2022-04-28 17:12:13 +0530

    adding sumNum function in index.js

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ function mulNum(num1,num2){
     return num1*num2;
 }
 
+function subNum(num1,num2){
+    return num1-num2;
+}
+
+
+
 console.log(addNum(5,6));
 console.log(mulNum(5,6));
+console.log(subNum(14,6));
