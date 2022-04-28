677d47ca4b190723424c9a36a05c406914493b23
GauravKohli170796 <89524181+GauravKohli170796@users.noreply.github.com>
2022-04-28 17:09:32 +0530

    adding divNum function in index.js

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function mulNum(num1,num2){
     return num1*num2;
 }
 
+function divNum(num1,num2){
+    return parseInt(num1/num2);
+}
+
 console.log(addNum(5,6));
 console.log(mulNum(5,6));
+console.log(divNum(15,3));
